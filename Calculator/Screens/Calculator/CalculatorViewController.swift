d56bcd567bce3e786cbceb0801eb38d24c1ab3e0
slash585 <slash585@gmail.com>
2025-04-15 23:35:42 +0300

    Init app

---
+++ Calculator/Screens/Calculator/CalculatorViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Mehmet Ali Özdemir on 15.04.2025.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
