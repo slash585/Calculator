87656abbf4fb2b045ec909eca9eba5ed142bbc77
slash585 <slash585@gmail.com>
2025-04-16 20:29:36 +0300

    Connect ui elements to view controller

--- Calculator/Screens/Calculator/CalculatorViewController.swift
+++ Calculator/Screens/Calculator/CalculatorViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 final class ViewController: UIViewController {
+    
+    //MARK: - Properties
 
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func handleTappedCalculatorButton(_ sender: UIButton) {
+        switch sender.tag {
+        case 0...9: // Numbers
+            print("")
+        case 10: // Clear
+            print("")
+        case 11: // Plus/Minus
+            print("")
+        case 12: // Percent
+            print("")
+        case 13: // Division
+            print("")
+        case 14: // Multiplication
+            print("")
+        case 15: // Subtraction
+            print("")
+        case 16: // Addition
+            print("")
+        case 17: // Equals
+            print("")
+        case 18: // Comma
+            print("")
+        default:
+            break
+        }
+    }
+    
+    //MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+}
 
+//MARK: - Private Methods
 
+private extension ViewController {
+    
 }
 
